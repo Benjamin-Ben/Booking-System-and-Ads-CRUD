08e70a289c457a32507280a8d8b1af54a4e0533d
Benjamin-Ben <42369601+Benjamin-Ben@users.noreply.github.com>
2019-05-09 14:51:55 +0200

    Made it so you can edit images

--- routes/post_routes.js
+++ routes/post_routes.js
@@ -50,13 +50,14 @@ module.exports = function (app) {
 
     // ----------- Update Ad ----------- //
 
+    // Update Text inputs
     app.post('/admin/ads/edit/:id', (req, res, next) => {
         let success = true;
-        let errorMessage;
+        let errorMessage1;
     
         if( !req.fields.title || !req.fields.description || !req.fields.price ) {
             success = false;
-            errorMessage = 'Et eller flere felter var tomme';
+            errorMessage1 = 'Et eller flere felter var tomme';
         }
     
         if( success === true ) {
@@ -79,7 +80,55 @@ module.exports = function (app) {
                     console.error(err);
                 }
     
-                res.render( 'admin_ads_edit', { results1, errorMessage, ...req.fields } );
+                res.render( 'admin_ads_edit', { results1, errorMessage1, ...req.fields } );
+    
+            });
+                
+        }
+    });
+
+    app.post('/admin/ads/edit-img/:imgName', (req, res, next) => {
+
+        let success = true;
+        let errorMessage2;
+    
+        if( !req.files.photo || !req.fields.photo_alt ) {
+            success = false;
+            errorMessage2 = 'Et eller flere felter var tomme';
+        }
+            
+        else if( req.files.photo.type.indexOf('image') === -1 ) {
+            success = false;
+            errorMessage2 = 'Du kan kun uploade billede filler';
+        }
+    
+        if( success === true ) {
+            const image = req.files.photo;
+            const renammedFile = `${Date.now()}_${image.name}`;
+        
+            fs.readFile(image.path, (err, data) => {
+                if (err) throw (err);
+                fs.writeFile(`./public/img/${renammedFile}`, data, err => {
+                    if (err) throw (err);
+
+                    fs.unlink( `./public/img/${[req.params.imgName]}`, (err, imgResults) => {
+                        if (err) { 
+                            res.render(viewTemplateError, { err }); 
+                        }
+                    });
+
+                    db.query(`UPDATE office_ads 
+                    SET img_src = ?, img_alt = ? WHERE office_ads.img_src = ?;`, [ renammedFile, req.fields.photo_alt, req.params.imgName ], (err, result) => {
+                        if (err) throw (err);
+                        res.redirect( '/admin/ads' );
+                    });
+                });
+            });
+        } else {
+            db.query( `SELECT * FROM office_ads WHERE office_ads.id = ?`, ( err, results1 ) => {
+                if (err) throw (err);
+    
+                res.render( 'admin_ads_edit', { results1, errorMessage2, ...req.fields } );
     
             });
                 
