caab71bc5e3fa10ed781de13aa3f743547eb5e6e
Benjamin-Ben <42369601+Benjamin-Ben@users.noreply.github.com>
2019-05-09 08:36:39 +0200

    Added the codebase

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+# Changelog - Ben NodeJS Kodebase
+#### Denne changelog bruger [Semantic Versioning](https://semver.org/spec/v2.0.0.html)
+
+---
+
+## Versions
+
+
+
+### [0.1.0 - 2019-05-09](###-0.1.0-|-2019-05-09)
+
+---
+
+## Unreleased
+
+---
+
+## Released
+
+### 0.1.0 | 2019-05-09
+
+* Tilføjet min kodebase til projektet
+
+---
+
---
+++ routes/get_routes.js
@@ -0,0 +1,153 @@
+const db = require('../config/mysql')();
+
+module.exports = function (app) {
+    
+    // =============================== READ =============================== //
+    const readRoutes = {
+
+        // Getting content without any SQL Queries
+        benNoSqlGet: function ( urlName, viewTemplate, pageTitle ) {
+            app.get( urlName, (req, res, next) => {
+                res.render( viewTemplate, { 'title': pageTitle } );
+            });
+        },
+        
+        // Getting content with a tottaly normal get route
+        benNormalGet: function ( urlName, viewTemplateError, viewTemplate, pageTitle, sqlQuery1, sqlQuery2, sqlQuery3, sqlQuery4, sqlQuery5, sqlQuery6 ) {
+            app.get( urlName, ( req, res, next ) => {
+    
+                    db.query(sqlQuery1, ( err, results1 ) => {
+                        if (err) { 
+                            res.render(viewTemplateError, { err }); 
+                        }
+    
+                        db.query(sqlQuery2, ( err, results2 ) => {
+                            if (err) { 
+                                res.render(viewTemplateError, { err }); 
+                            }
+    
+                            db.query(sqlQuery3, ( err, results3 ) => {
+                                if (err) { 
+                                    res.render(viewTemplateError, { err }); 
+                                }
+    
+                                db.query(sqlQuery4, ( err, results4 ) => { 
+                                    if (err) { 
+                                        res.render(viewTemplateError, { err }); 
+                                    }
+    
+                                    db.query(sqlQuery5, ( err, results5 ) => {
+                                        if (err) { 
+                                            res.render(viewTemplateError, { err }); 
+                                        }
+    
+                                        db.query(sqlQuery6, ( err, results6 ) => {
+                                            if (err) { 
+                                                res.render(viewTemplateError, { err }); 
+                                            }
+    
+                                            res.render(viewTemplate, { 'title': pageTitle, results1, results2, results3, results4, results5, results6 });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            }, // End of 'benNormalGet'
+    
+        benParamsGet: function ( urlName, viewTemplateError, viewTemplate, pageTitle, sqlQuery1, sqlQuery2, sqlQuery3, sqlQuery4, sqlQuery5, sqlQuery6 ) {
+            app.get( urlName, ( req, res, next ) => {
+    
+                    db.query(sqlQuery1, [req.params.id], ( err, results1 ) => {
+                        if (err) { 
+                            res.render(viewTemplateError, { err }); 
+                        }
+    
+                        db.query(sqlQuery2, ( err, results2 ) => {
+                            if (err) { 
+                                res.render(viewTemplateError, { err }); 
+                            }
+    
+                            db.query(sqlQuery3, ( err, results3 ) => {
+                                if (err) { 
+                                    res.render(viewTemplateError, { err }); 
+                                }
+    
+                                db.query(sqlQuery4, ( err, results4 ) => { 
+                                    if (err) { 
+                                        res.render(viewTemplateError, { err }); 
+                                    }
+    
+                                    db.query(sqlQuery5, ( err, results5 ) => {
+                                        if (err) { 
+                                            res.render(viewTemplateError, { err }); 
+                                        }
+    
+                                        db.query(sqlQuery6, ( err, results6 ) => {
+                                            if (err) { 
+                                                res.render(viewTemplateError, { err }); 
+                                            }
+    
+                                            res.render(viewTemplate, { 'title': pageTitle, results1, results2, results3, results4, results5, results6 });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            }, // End of 'benParamsGet'
+    
+    
+        benSearchGet: function ( urlName, viewTemplateError, viewTemplate, pageTitle, sqlQuery1, sqlQuery2, sqlQuery3, sqlQuery4, sqlQuery5, sqlQuery6 ) {
+                app.get( urlName, ( req, res, next ) => {
+        
+                        db.query(sqlQuery1, [`%${req.query.search_query}%`, `%${req.query.search_query}%`, `%${req.query.search_query}%`, `%${req.query.search_query}%`, `%${req.query.search_query}%`, `%${req.query.search_query}%`], ( err, results1 ) => {
+                            if (err) { 
+                                res.render(viewTemplateError, { err }); 
+                            }
+        
+                            db.query(sqlQuery2, ( err, results2 ) => {
+                                if (err) { 
+                                    res.render(viewTemplateError, { err }); 
+                                }
+        
+                                db.query(sqlQuery3, ( err, results3 ) => {
+                                    if (err) { 
+                                        res.render(viewTemplateError, { err }); 
+                                    }
+        
+                                    db.query(sqlQuery4, ( err, results4 ) => { 
+                                        if (err) { 
+                                            res.render(viewTemplateError, { err }); 
+                                        }
+        
+                                        db.query(sqlQuery5, ( err, results5 ) => {
+                                            if (err) { 
+                                                res.render(viewTemplateError, { err }); 
+                                            }
+        
+                                            db.query(sqlQuery6, ( err, results6 ) => {
+                                                if (err) { 
+                                                    res.render(viewTemplateError, { err }); 
+                                                }
+    
+                                                res.render(viewTemplate, { 'title': pageTitle, results1, results2, results3, results4, results5, results6 });
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+            } // End of 'benSearchGet'
+
+    }
+    
+    // ======================= HOME ======================= //
+    readRoutes.benNoSqlGet(
+        '/', 'home', 'Home Page'
+    );
+            
+} // End of 'Module.Exports'
